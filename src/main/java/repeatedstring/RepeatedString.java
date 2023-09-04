06e4da26618708a5ccbe150c7b5cca0dab3f91de
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-09-04 10:46:44 +0100

    Repeated String challenge: refactored

--- src/main/java/repeatedstring/RepeatedString.java
+++ src/main/java/repeatedstring/RepeatedString.java
@@ -3,35 +3,34 @@ package repeatedstring;
 public class RepeatedString {
 
     public long repeatedString(String s, long n) {
-        int nRepetition = (int) n;
         long result = 0;
-        int stringLength = s.length();
+        long stringLength = s.length();
         if (stringLength >= n){
-            // easy case
-            var substring = s.substring(0,nRepetition);
-            result = substring.chars()
-                    .filter( ch -> ch =='a')
-                    .count();
+            // easy case:  string is longer than n
+            var substring = s.substring(0,(int)n);
+            result = getCount(substring);
         } else {
-            // complex case
-            int fullRepeatedTimes = nRepetition / stringLength;
-            int incompleteRepetition = nRepetition % stringLength;
-
-            long count = s.chars()
-                    .filter( ch -> ch =='a')
-                    .count();
+            // complex case: string is shorter than n
+            // find number of time string needs to be repeated
+            long fullRepeatedTimes = n / stringLength;
+            // find number of chars of last repetition
+            int incompleteRepetition = (int) (n % stringLength);
+            // count whole repetitions
+            long count = getCount(s);
             result = count * fullRepeatedTimes;
-
+            // If last repetition is incomplete, only consider fewer the relevant chars
             if (incompleteRepetition > 0){
-                var substrIncompleteCount = s.substring(0,incompleteRepetition);
-                long incompleteSubstrCount = substrIncompleteCount.chars()
-                        .filter( ch -> ch =='a')
-                        .count();
+                var substrIncompleteCount = s.substring(0, incompleteRepetition);
+                long incompleteSubstrCount = getCount(substrIncompleteCount);
                 result = result + incompleteSubstrCount;
             }
-
         }
-
         return result;
+    }
+
+    private long getCount(String str) {
+        return str.chars()
+                .filter( ch -> ch =='a')
+                .count();
     }
 }
