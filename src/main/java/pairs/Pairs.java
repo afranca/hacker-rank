36ea4b9905b68a165638491b28674935a13be0e8
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-09-09 18:07:53 +0100

    Pair challenge - difference between pairs of an array

---
+++ src/main/java/pairs/Pairs.java
@@ -0,0 +1,22 @@
+package pairs;
+
+import java.util.List;
+
+public class Pairs {
+
+    public int pairs(int k, List<Integer> arr) {
+        int pairCounter=0;
+        int current;
+        int next;
+        for(int i=0; i < arr.size()-1; i++){
+            current = arr.get(i);
+            for(int j=i+1; j < arr.size(); j++){
+                next = arr.get(j);
+                if( (current-next == k) || (next-current==k)){
+                    pairCounter++;
+                }
+            }
+        }
+        return pairCounter;
+    }
+}
---
+++ src/test/java/pairs/PairsTest.java
@@ -0,0 +1,45 @@
+package pairs;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PairsTest {
+
+    Pairs sut = new Pairs();
+
+    @Test
+    void simpleCase(){
+        List arr = List.of(1,5,3,4,2);
+        int k = 2;
+        int actual = sut.pairs(k,arr);
+        assertEquals(3, actual);
+    }
+
+    @Test
+    void minArray_with1Match(){
+        List arr = List.of(3,5);
+        int k = 2;
+        int actual = sut.pairs(k,arr);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void minArray_withNoMatch(){
+        List arr = List.of(1,5);
+        int k = 2;
+        int actual = sut.pairs(k,arr);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void arrayWithNegativeNumbers(){
+        List arr = List.of(-1,5,-3,3,-2);
+        int k = -2;
+        int actual = sut.pairs(k,arr);
+        assertEquals(2, actual);
+    }
+
+}
