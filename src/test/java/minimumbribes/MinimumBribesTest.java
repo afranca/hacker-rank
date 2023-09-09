0eebc408b9005fbf0f5f0bf8454b09613702016b
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-09-09 10:09:09 +0100

    edge cases for the Bribery challenge

--- src/test/java/minimumbribes/MinimumBribesTest.java
+++ src/test/java/minimumbribes/MinimumBribesTest.java
@@ -11,23 +11,39 @@ class MinimumBribesTest {
     MinimumBribes sut;
 
     @Test
-    void defaultTest(){
+    void oneSingleBribe_atTheEnd(){
         List<Integer> testInput = List.of(1,2,4,5,3);
-        sut = new MinimumBribes();
-        int ret = sut.minimumBribes(testInput);
-        assertEquals(2,ret);
-    }
-
-    @Test
-    void shouldReturn1(){
-        List<Integer> testInput = List.of(1,2,4,3,5);
         sut = new MinimumBribes();
         int ret = sut.minimumBribes(testInput);
         assertEquals(1,ret);
     }
 
     @Test
-    void shouldReturn0(){
+    void oneSingleBribe_atTheBeginning(){
+        List<Integer> testInput = List.of(2,1,3,4,5);
+        sut = new MinimumBribes();
+        int ret = sut.minimumBribes(testInput);
+        assertEquals(1,ret);
+    }
+
+    @Test
+    void oneDoubleBribe_atTheEnd(){
+        List<Integer> testInput = List.of(1,2,5,3,4);
+        sut = new MinimumBribes();
+        int ret = sut.minimumBribes(testInput);
+        assertEquals(2,ret);
+    }
+
+    @Test
+    void oneDoubleBribe_atTheBeginning(){
+        List<Integer> testInput = List.of(3,1,2,4,5);
+        sut = new MinimumBribes();
+        int ret = sut.minimumBribes(testInput);
+        assertEquals(2,ret);
+    }
+
+    @Test
+    void noBribes(){
         List<Integer> testInput = List.of(1,2,3,4,5,6,7,8,9);
         sut = new MinimumBribes();
         int ret = sut.minimumBribes(testInput);
@@ -35,7 +51,7 @@ class MinimumBribesTest {
     }
 
     @Test
-    void shouldReturn0_shortList(){
+    void noBribes_shortList(){
         List<Integer> testInput = List.of(1,2);
         sut = new MinimumBribes();
         int ret = sut.minimumBribes(testInput);
@@ -43,26 +59,67 @@ class MinimumBribesTest {
     }
 
     @Test
-    void shouldReturn1_shortList(){
+    void oneSingleBribe_shortList(){
         List<Integer> testInput = List.of(2,1);
         sut = new MinimumBribes();
         int ret = sut.minimumBribes(testInput);
         assertEquals(1,ret);
     }
 
+
     @Test
-    void shouldReturn3(){
-        List<Integer> testInput = List.of(1,4,2,5,3);
+    void twoDoubleBribes_case02(){
+        List<Integer> testInput = List.of(3,1,5,2,4);
         sut = new MinimumBribes();
         int ret = sut.minimumBribes(testInput);
         assertEquals(3,ret);
     }
 
     @Test
-    void shouldReturn4(){
+    void oneSingleBribe_oneDoubleBribe_case01(){
+        List<Integer> testInput = List.of(2,4,1,3);
+        sut = new MinimumBribes();
+        int ret = sut.minimumBribes(testInput);
+        assertEquals(3,ret);
+    }
+
+    @Test
+    void oneSingleBribe_oneDoubleBribe_case02(){
+        List<Integer> testInput = List.of(1,3,5,2,4,6);
+        sut = new MinimumBribes();
+        int ret = sut.minimumBribes(testInput);
+        assertEquals(3,ret);
+    }
+
+    @Test
+    void twoDoubleBribe_case01(){
+        List<Integer> testInput = List.of(3,1,5,2,4,6);
+        sut = new MinimumBribes();
+        int ret = sut.minimumBribes(testInput);
+        assertEquals(4,ret);
+    }
+
+    @Test
+    void twoDoubleBribe_case02(){
         List<Integer> testInput = List.of(1,4,5,2,3);
         sut = new MinimumBribes();
         int ret = sut.minimumBribes(testInput);
         assertEquals(4,ret);
+    }
+
+    @Test
+    void threeDoubleBribe(){
+        List<Integer> testInput = List.of(3,1,5,6,2,4);
+        sut = new MinimumBribes();
+        int ret = sut.minimumBribes(testInput);
+        assertEquals(6,ret);
+    }
+
+    @Test
+    void oneDoubleBribe_oneSingleBribe(){
+        List<Integer> testInput = List.of(1,4,2,5,3);
+        sut = new MinimumBribes();
+        int ret = sut.minimumBribes(testInput);
+        assertEquals(3,ret);
     }
 }
