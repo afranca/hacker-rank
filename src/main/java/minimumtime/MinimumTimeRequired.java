7d01327ab31bce50ba7b7814358568ed6174e21c
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-09-13 07:57:11 +0100

    Minimum Time Required challenge: Binary Search implementation

--- src/main/java/minimumtime/MinimumTimeRequired.java
+++ src/main/java/minimumtime/MinimumTimeRequired.java
@@ -1,14 +1,14 @@
 package minimumtime;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MinimumTimeRequired {
 
     public long minTime(long[] machines, long goal) {
         long itemCount=0;
         long dayCount=0;
-        System.out.println("Day  Count:"+dayCount);
-        System.out.println("Item Count:"+itemCount);
-        System.out.println("* * * * * * * * * * * * * * * ");
-        System.out.println("");
         while(itemCount!=goal){
             dayCount++;
             for(int i=0;i<machines.length;i++){
@@ -16,24 +16,51 @@ public class MinimumTimeRequired {
                 long rest = dayCount % current;
                 if (rest == 0){
                     itemCount++;
-                    System.out.println("Day  Count:"+dayCount);
-                    System.out.println("Item Count:"+itemCount);
-                    System.out.println("Goal Count:"+goal);
-                    System.out.println("* * * * * * * * * * * * * * * ");
-                    System.out.println("");
+
                 }
             }
         }
         return dayCount;
     }
-}
 
-/*
-        long fastestMachine = machines[0];
-        for(int i=1;i<machines.length;i++){
-            long current = machines[i];
-            if (current < fastestMachine){
-                fastestMachine = current;
+
+    public long minTimeBinary(long[] machines, long goal) {
+        Arrays.sort(machines); // Sort the machines in ascending order
+        long left = 1; // Minimum time required
+        long right = machines[machines.length - 1] * goal; // Maximum time required
+
+        while (left < right) {
+            long mid = (left + right) / 2; // Calculate the mid-point
+
+            long itemsProduced = 0;
+            for (int i = 0; i < machines.length; i++) {
+                itemsProduced += mid / machines[i]; // Calculate items produced by each machine at time 'mid'
+            }
+
+            if (itemsProduced < goal) {
+                left = mid + 1; // Adjust the left bound
+            } else {
+                right = mid; // Adjust the right bound
             }
         }
- */
+
+        return left; // 'left' will be the minimum time required
+    }
+
+    public long minTimeBinaryArrays(long[] machines, long goal) {
+        Arrays.sort(machines); // Sort the machines in ascending order
+        long right = machines[machines.length - 1] * goal; // Maximum time required
+
+        int[] days = new int[(int)right];
+        for(int i=0;i<right;i++){
+            days[i]=i+1;
+        }
+
+        System.out.println(Arrays.binarySearch(days, (int) goal));
+        int index = Arrays.binarySearch(days, (int) goal);
+
+        return days[index]; // 'left' will be the minimum time required
+    }
+
+}
+
