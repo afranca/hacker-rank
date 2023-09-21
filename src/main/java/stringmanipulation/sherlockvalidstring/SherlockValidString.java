2820b4cf421e1f07ff3985c379fb3243917c8cbb
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-09-21 08:23:00 +0100

    Fixing solution plus adding second implementation which was mpt done by me but still uses to hashmaps to determine the occurrences and the frequencies, basically the same idea

--- src/main/java/stringmanipulation/sherlockvalidstring/SherlockValidString.java
+++ src/main/java/stringmanipulation/sherlockvalidstring/SherlockValidString.java
@@ -1,43 +1,123 @@
 package stringmanipulation.sherlockvalidstring;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SherlockValidString {
 
     public String isValid(String s) {
-        // Write your code here
-        HashMap<String, Integer> counterMap = new HashMap<>();
+        // Create Hash with counts of all letters
+        HashMap<String, Integer> fullCounterMap = new HashMap<>();
         char arr[] = s.toCharArray();
         for(int i = 0; i<arr.length; i++ ){
             char letter = arr[i];
             String letterStr = String.valueOf(letter);
-            Integer counter = counterMap.get(letterStr);
+            Integer counter = fullCounterMap.get(letterStr);
             if (counter != null){
                 counter++;
             } else {
-                counter=0;
+                counter=1;
             }
-            counterMap.put(letterStr, counter);
+            fullCounterMap.put(letterStr, counter);
         }
 
-        HashSet<Integer> counterSet = new HashSet<>();
-        counterMap.forEach( (key, value) -> {
-                counterSet.add(value);
+        // Create Hash with counts of letter occurrences
+        HashMap<Integer, Integer> aggregatedCounterMap = new HashMap<>();
+        fullCounterMap.forEach( (fullKey, aggregatedKey) -> {
+                Integer aggregatedValue = aggregatedCounterMap.get(aggregatedKey);
+                if (aggregatedValue == null){
+                    aggregatedCounterMap.put(aggregatedKey, 1);
+                } else {
+                    aggregatedCounterMap.put(aggregatedKey, ++aggregatedValue);
+                }
             }
         );
 
-        if (counterSet.size() > 2){
+        // If there is only 0ne, it's already a valid string
+        if (aggregatedCounterMap.size() == 1){
+            return  "YES";
+        }
+
+        // If there are more than 2 counters, bail out
+        if (aggregatedCounterMap.size() > 2){
             return  "NO";
-        } else {
-            Integer counterArr[] = new Integer[2];
-            counterSet.toArray(counterArr);
-            if ( (counterArr[0] -1 == counterArr[1]) || (counterArr[1]-1 == counterArr[0])  ){
-                return  "YES";
+        }
+
+
+        // Iterate through aggregated Map
+        Integer keys[] = new Integer[2];
+        Integer values[] = new Integer[2];
+        AtomicInteger counter = new AtomicInteger(0);
+
+        aggregatedCounterMap.forEach( (key, value) -> {
+            keys[counter.get()] = key;
+            values[counter.get()] = value;
+            counter.incrementAndGet();
+        });
+
+        // Deciding which occurrence is bigger and subtract them (bigger - smaller)
+        if (keys[0] > keys[1]){
+            // Then find out if difference of the number of repetitions equals to 1
+            if (keys[0] - keys[1] == 1){
+                // Then find out if one occurred only once
+                if (values[0] == 1 || values[1] == 1){
+                    return "YES";
+                }
+            } else {
+                return "NO";
+            }
+
+        } else if (keys[1] > keys[0]){
+            if (keys[1] - keys[0] == 1){
+                if (values[1] == 1 || values[0] == 1){
+                    return  "YES";
+                }
+            } else {
+                return "NO";
             }
         }
-        return  "NO";
-
-
-
+        return "NO";
     }
+
+    public String isValid2(String s) {
+        // Create a frequency map of characters in the string
+        Map<Character, Integer> charFrequency = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
+        }
+
+        // Create a frequency map of frequencies
+        Map<Integer, Integer> frequencyCount = new HashMap<>();
+        for (int freq : charFrequency.values()) {
+            frequencyCount.put(freq, frequencyCount.getOrDefault(freq, 0) + 1);
+        }
+
+        // If there is only one distinct frequency, it's already valid
+        if (frequencyCount.size() == 1) {
+            return "YES";
+        }
+
+        // If there are two distinct frequencies, check if it's possible to remove one character
+        if (frequencyCount.size() == 2) {
+            int freq1 = -1, freq2 = -1;
+            for (int freq : frequencyCount.keySet()) {
+                if (freq1 == -1) {
+                    freq1 = freq;
+                } else {
+                    freq2 = freq;
+                }
+            }
+
+            // Check if one of the frequencies occurs only once and can be removed
+            if ((Math.abs(freq1 - freq2) == 1 && (frequencyCount.get(freq1) == 1 || frequencyCount.get(freq2) == 1))
+                    || (freq1 == 1 && frequencyCount.get(freq1) == 1)
+                    || (freq2 == 1 && frequencyCount.get(freq2) == 1)) {
+                return "YES";
+            }
+        }
+
+        // If none of the conditions are met, it's not possible to make it valid
+        return "NO";
+    }
+
 }
--- src/test/java/stringmanipulation/sherlockvalidstring/SherlockValidStringTest.java
+++ src/test/java/stringmanipulation/sherlockvalidstring/SherlockValidStringTest.java
@@ -17,9 +17,110 @@ class SherlockValidStringTest {
     }
 
     @Test
-    void isValid() {
+    void isValid_FailedCase04() {
+        String s = "aabbc";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_FailedCase05() {
+        String s = "aaaaabc";
+        String result = sut.isValid(s);
+        assertEquals(NO, result);
+    }
+
+    @Test
+    void isValid_FailedCase17() {
         String s = "aabbccddeefghi";
         String result = sut.isValid(s);
         assertEquals(NO, result);
     }
+
+    @Test
+    void isValid_FailedCase18() {
+        String s = "abcdefghhgfedecba";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_FailedCase16() {
+        String s = "abbac";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+
+
+    @Test
+    void isValid_testCase01() {
+        String s = "abb";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_testCase02() {
+        String s = "aab";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_testCase03() {
+        String s = "aaabbbcccefg";
+        String result = sut.isValid(s);
+        assertEquals(NO, result);
+    }
+
+    @Test
+    void isValid_testCase04() {
+        String s = "aaabbbfg";
+        String result = sut.isValid(s);
+        assertEquals(NO, result);
+    }
+
+    @Test
+    void isValid_testCase05() {
+        String s = "aaabbb";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_testCase06() {
+        String s = "aabb";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_testCase07() {
+        String s = "ab";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_testCase08() {
+        String s = "aa";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_testCase09() {
+        String s = "a";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
+    @Test
+    void isValid_testCase10() {
+        String s = "ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd";
+        String result = sut.isValid(s);
+        assertEquals(YES, result);
+    }
+
 }
