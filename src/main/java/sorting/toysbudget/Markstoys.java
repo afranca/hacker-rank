f0ddffe2e715d1a7a72bbaad8a46903612f97244
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-09-26 08:13:35 +0100

    Mark & Toys challenge: maximizing the budget

---
+++ src/main/java/sorting/toysbudget/Markstoys.java
@@ -0,0 +1,12 @@
+package sorting.toysbudget;
+
+import java.util.List;
+
+public class Markstoys {
+
+    public  int maximumToys(List<Integer> prices, int k) {
+        // Write your code here
+        return 0;
+    }
+
+}
