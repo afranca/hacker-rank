57c391f22d138101a05182392b369e6c75c2a895
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-09-28 08:34:36 +0100

    Mark & Toy challenge: simple solution with one loop and sorted array

--- src/main/java/sorting/toysbudget/Markstoys.java
+++ src/main/java/sorting/toysbudget/Markstoys.java
@@ -1,34 +1,32 @@
 package sorting.toysbudget;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Markstoys {
 
     public  int maximumToys(List<Integer> prices, int k) {
-        int wallet = k;
-        int maxToysCounter=0;
-
-        // Create List into array, sort array ascending
+        // Convert List into array, sort array ascending
         Integer arr[] = new Integer[prices.size()];
         arr = prices.toArray(arr);
         Arrays.sort(arr);
 
+        int wallet = k;
+        int toysCounter = 0;
+
+        // Iterates the prices array until the budget is finished and return count
         for(int i=0; i < arr.length -1; i++){
-            int toysCounter = 0;
-            for(int j=i; j < arr.length -1; j++){
-                   wallet = wallet - arr[j];
-                   if (wallet >= 0){
-                        toysCounter++;
-                   }
-            }
-            if (toysCounter > maxToysCounter){
-                maxToysCounter = toysCounter;
-            }
+           wallet = wallet - arr[i];
+           if (wallet >= 0){
+                toysCounter++;
+           } else {
+               break;
+           }
         }
 
-        return maxToysCounter;
+        return toysCounter;
     }
+
+
 
 }
