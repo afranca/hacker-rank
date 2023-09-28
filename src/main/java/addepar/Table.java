f92a9eda607acf8eb2b74a8c535d7bfe4960c76d
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-09-28 20:32:50 +0100

    Adepar Hacker Rank challenge: using an Action class to keep track of all uncommitted changes to a tempTable. Once transaction is commited, apply all changes to the original table

--- src/main/java/addepar/Table.java
+++ src/main/java/addepar/Table.java
@@ -1,39 +1,75 @@
 package addepar;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 
 public class Table {
 
     public boolean isTransactionStarted = false;
     Map<String, Map<String, String>> originalTable = new HashMap<>();
-    Map<String, Map<String, String>> changedTable = new HashMap<>();
+    Map<String, Map<String, String>> tempTable = new HashMap<>();
 
+    List<Action> actions = new ArrayList<>();
 
-    List<String> createActions = new ArrayList<>();
-    List<String> deleteActions = new ArrayList<>();
+    //List<String> createActions = new ArrayList<>();
+    //List<String> deleteActions = new ArrayList<>();
     ////////////////////////////////////////
     // Data operations
 
     public void createRow(String rowName) {
-        createActions.add(rowName);
+        //createActions.add(rowName);
+        if (isTransactionStarted){
+            tempTable.put(rowName, new HashMap<String, String>());
+            actions.add(new Action("create",rowName));
+        } else {
+            originalTable.put(rowName, new HashMap<String, String>());
+        }
     }
 
     public void deleteRow(String rowName) {
-        deleteActions.add(rowName);
+        //deleteActions.add(rowName);
+        if (isTransactionStarted){
+            tempTable.remove(rowName);
+            actions.add(new Action("delete",rowName));
+        } else {
+            originalTable.remove(rowName);
+        }
+
     }
 
     public void updateCell(String rowName, String columnName, String newVal) {
+        //System.out.println("** updating **");
         Map<String, String> changedRow = new HashMap<>();
         changedRow.put(columnName, newVal);
-        changedTable.put(rowName, changedRow);
-
+        if (isTransactionStarted){
+            tempTable.put(rowName, changedRow);
+            actions.add(new Action("update",rowName, columnName, newVal));
+        } else {
+            originalTable.put(rowName, changedRow);
+        }
     }
+
     public void showTable() {
+        String status ;
+        Map<String, Map<String, String>> tableToShow;
+        if (isTransactionStarted){
+            tableToShow = tempTable;
+            status = "Uncommitted";
+        } else {
+            tableToShow = originalTable;
+            status = "Committed";
+        }
+
+        System.out.print("Table ("+status+"): ");
+        for (Map.Entry<String, Map<String, String>> row : tableToShow.entrySet()) {
+            String rowName = row.getKey();
+            Map<String, String> columnNames = row.getValue();
+            System.out.print(rowName + ": ");
+            for (Map.Entry<String, String> column : columnNames.entrySet()) {
+                System.out.print("(" + column.getKey() + ", " + column.getValue() + ") ");
+            }
+            System.out.println();
+        }
 
     }
 
@@ -45,20 +81,22 @@ public class Table {
     }
 
     public void commitTransaction() {
-        for(String rowName : createActions){
-            originalTable.put(rowName, null);
+        for(Action action : actions){
+            if (action.name.equalsIgnoreCase("create")){
+                originalTable.put(action.rowName, new HashMap<String, String>());
+            } else if (action.name.equalsIgnoreCase("delete")){
+                originalTable.remove(action.rowName);
+            } else {
+                Map<String, String> row = originalTable.get(action.rowName);
+                row.put(action.columnName, action.value);
+            }
         }
-
-        for(String rowName : deleteActions){
-            originalTable.remove(rowName);
-        }
-
-        Set<String> keys = changedTable.keySet();
-
+        isTransactionStarted = false;
     }
 
     public void rollbackTransaction() {
-
+        isTransactionStarted = false;
+        tempTable.clear();
     }
 
     public static void main(String args[] ) throws Exception {
@@ -71,8 +109,32 @@ public class Table {
         table.beginTransaction();
         table.updateCell("Row1", "Column2", "Value2");
         table.showTable();
-
-        table.commitTransaction();
+        table.updateCell("Row1", "Column1", "Value3");
         table.showTable();
+        table.updateCell("Row1", "Column1", "Value5");
+        table.showTable();
+        table.commitTransaction();
+
+        table.showTable();
+    }
+}
+
+class Action {
+
+    public String name;
+    public String rowName;
+    public String columnName;
+    public String value;
+
+    public Action (String actionName, String rowName){
+        this.name = actionName;
+        this.rowName = rowName;
+    }
+
+    public Action (String actionName, String rowName, String columnName, String value){
+        this.name = actionName;
+        this.rowName = rowName;
+        this.columnName = columnName;
+        this.value = value;
     }
 }
